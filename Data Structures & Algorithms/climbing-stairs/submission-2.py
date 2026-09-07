986aba581736fa46bfa38260d4a7cbee4b1a5501
Lâm Hưng Nguyên <126869306+Kussssssss@users.noreply.github.com>
2026-09-07 14:15:31 +0700

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        step1, step2 = 1, 1
+
+        for _ in range(n-1):
+            temp = step1 + step2
+            step1 = step2
+            step2 = temp
+        
+        return step2
+
+            
